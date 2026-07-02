97c78c00328c9b792fc44cfc6a4d96d2e3289910
Saido <saidahmed0602@gmail.com>
2026-07-02 11:03:57 +0300

    Implement dynamic add and remove elements feature

--- submission/js/script.js
+++ submission/js/script.js
@@ -59,5 +59,42 @@ services.forEach(function(service){
     
     //Appending (meaning  adding an element to the end of another element) the card to the services container
     servicesContainer.appendChild(card);
-    
+});
+
+//Feature 2: Dynamically adding and removing Elements by the user through an input button of 
+//using createElement() and appendChild()), and each item gets its own button to remove it (using remove()).
+//Finding the input element in HTML where the user will type the service name
+const input= document.getElementById("service-input");
+const button = document.getElementById("add-button");
+const list= document.getElementById("service-list");
+//Adding an event listener to the button so that when it is clicked, the function inside it will be executed
+//we have many events including click dblclick, mouseover, keydown ,keyup ,submit etc
+button.addEventListener("click",function(){
+    //Getting the value of the input element
+    const newService = input.value;
+    //Preventing the user from adding an empty service to the list
+    if(newService === ""){
+        alert("Kindly Enter a Service");
+        return;
+    }
+    console.log(newService);
+    //Creating a new li element for the new services, it creates <li></li> in the HTML
+    const listItem = document.createElement("li");
+    //Entering the value of the input element into the li element, it creates <li>newService</li> in the HTML
+    listItem.textContent = newService;
+    //Creating a new button element for the remove button, it creates <button></button> in the HTML
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    //Here we are putting the remove button inside the li element, not like list.appendChild(listItem); we put it inside the ul element, it creates <li>newService<button>Remove</button></li> in the HTML
+    listItem.appendChild(removeButton);
+    //Adding an event listener to the remove button so that when it is clicked, the function inside it will be executed
+    removeButton.addEventListener("click",function(){
+        listItem.remove();
+        
+    });
+    //Appending li element (adding it to the end of the ul element) to the ul element, it creates <ul><li>newService</li></ul> in the HTML
+    //we use list instead of listItem because we want to append the li element to the ul element
+    list.appendChild(listItem);
+    //Empting the textbox after the user has added a service, so that the user can add another service without having to delete the previous one
+    input.value = "";
 });
